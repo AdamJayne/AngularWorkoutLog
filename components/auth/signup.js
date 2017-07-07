4282001fee4fe64648804e7d3ebcc25d850e7a80
Adam Jayne <adamjayne402@gmail.com>
2017-07-07 13:01:53 -0400

    Working views and controllers

---
+++ components/auth/signup.js
@@ -0,0 +1,31 @@
+(function(){
+	angular
+		.module('workoutlog.auth.signup', ['ui.router'])
+		.config(signupConfig);
+
+		function signupConfig($stateProvider){
+			$stateProvider
+				.state('signup', {
+					url: '/signup',
+					templateUrl: '/components/auth/signup.html',
+					controller: SignUpController,
+					controllerAs: 'ctrl',
+					bindToController: this
+				});
+		}
+
+		signupConfig.$inject = ['$stateProvider'];
+
+		function SignUpController($state, UserService) {
+			var vm = this;
+			vm.user = {};
+			vm.message = "Sign up for an account!"
+			vm.submit = function() {
+				UserService.create(vm.user).then(function(response){
+					$state.go('define')
+				});
+			};
+		}
+
+		SignUpController.$inject = ['$state', 'UserService'];
+})();
